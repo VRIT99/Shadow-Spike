924cf48cc0acd0e3d2c3de00352c3e34ea4cd00f
Sahil Hiwale <sahilhiwale2003@gmail.com>
2026-07-27 00:33:47 +0530

    Fix API baseURL to use environment variable

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/v1',
+  baseURL: import.meta.env.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL}/api/v1`
+    : 'http://127.0.0.1:8000/api/v1',
   headers: { 'Content-Type': 'application/json' }
 })
 
